1b0ce269e9da51e22637e262529a396c85c5d489
Taras <andergraund420@gmail.com>
2023-07-10 18:43:51 +0300

    made the syntax everywhere more logical with minor style adjustments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const resizeButton = document.querySelector('#resize');
-resizeButton.addEventListener('click', createGrid);
-function createGrid() {
-    for (let i = 1; i <= 16; i++) {
+const createButton = document.querySelector('#create');
+createButton.addEventListener('click', () => {
+    let size = +prompt('Enter the preferred size: ');
+    createGrid(size);
+});
+function createGrid(size) {
+    for (let i = 1; i <= size; i++) {
         const container = document.createElement('div');
         container.classList.add('container');
         document.body.appendChild(container);
-        for (let x = 1; x <= 16; x++) {
+        for (let x = 1; x <= size; x++) {
             const div = document.createElement('div');
             div.classList.add('grid');
             container.appendChild(div);
