e90bb4e9719f9d3b7086b38bea7f8573576694fd
Taras <andergraund420@gmail.com>
2023-07-10 18:48:45 +0300

    now when new grid is created, the old one disappears!

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//Make the grid size itself accordingly to available width and height of the window
+//Create a hover effect which will make squares change color
+const mainContainer = document.querySelector('#main-container');
 const createButton = document.querySelector('#create');
 createButton.addEventListener('click', () => {
+    mainContainer.replaceChildren();
     let size = +prompt('Enter the preferred size: ');
     createGrid(size);
 });
@@ -7,7 +11,7 @@ function createGrid(size) {
     for (let i = 1; i <= size; i++) {
         const container = document.createElement('div');
         container.classList.add('container');
-        document.body.appendChild(container);
+        mainContainer.appendChild(container);
         for (let x = 1; x <= size; x++) {
             const div = document.createElement('div');
             div.classList.add('grid');
