d260282172c9adfe475470ff711cb436035ce3c2
Taras <andergraund420@gmail.com>
2023-07-12 00:41:33 +0300

    resize button is now back, more comments and refactoring

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+//Get mainContainer for future use
+const mainContainer = document.querySelector('#main-container');
+//Create buttonDiv and resizeButton for simpleUI
+const buttonDiv = document.createElement('div');
+const resizeButton = document.createElement('button');
+//Style buttonDiv and resizeButton
+buttonDiv.style.width = `${window.innerWidth}`;
+resizeButton.textContent = 'create';
+buttonDiv.classList.add('button-div');
+resizeButton.classList.add('resize');
+//Append resizeButton to buttonDiv
+buttonDiv.appendChild(resizeButton);
+//Insert buttonDiv at the start of body element
+document.body.insertBefore(buttonDiv, mainContainer);
+
 //Function to get grid size from the user
 function getSize() {
     //Prompt user for size
-    const size = +prompt('Enter the preferred size: ');
+    const size = +prompt('Enter the desirable grid size between 2 and 100');
 
     //If size is bigger than 100 or less than 2
     if (size > 100 || size < 2) {
@@ -13,18 +28,21 @@ function getSize() {
     }
 }
 
-//Initialize getSize function
-getSize();
+//Add event listener to resizeButton so grid could be initialized
+resizeButton.addEventListener('click', () => {
+    //Clear old grid
+    mainContainer.replaceChildren();
+    //Re-prompt for size of new grid
+    getSize();
+});
 
 //Function to create the grid
 function createGrid(size) {
     //Create grid vertically
     for (let i = 1; i <= size; i++) {
-        const mainContainer = document.querySelector('#main-container');
-
         //Calculate width and height for each grid square and convert it into viewport units
         const gridWidth  = ((window.innerWidth / size) / window.innerWidth) * 100 + 'vw';
-        const gridHeight = ((window.innerHeight / size) / window.innerHeight) * 100 + 'vh';
+        const gridHeight = (((window.innerHeight - buttonDiv.offsetHeight) / size) / window.innerHeight) * 100 + 'vh';
 
         //Create vertical container for grid squares
         const container = document.createElement('div');
@@ -38,7 +56,6 @@ function createGrid(size) {
 
         //Create grid horizontally
         for (let x = 1; x <= size; x++) {
-
             //Create grid squares horizontally
             const div = document.createElement('div');
 
@@ -50,21 +67,21 @@ function createGrid(size) {
             container.appendChild(div);
         }
     }
+
+    //Get node list of all grid squares
+    const divs = document.querySelectorAll('.grid');
+
+    //Iterate through node list
+    divs.forEach((div) => {
+        //Add event listener for hover on each square
+        div.addEventListener('mouseover', () => {
+            //Change background color of square to random RGB value
+            div.style.backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+        });
+    });
 }
 
 //Function to get random number in RGB range
 function getRandomNumber() {
     return Math.round(Math.random() * 255);
 }
-
-//Get node list of all grid squares
-const divs = document.querySelectorAll('.grid');
-
-//Iterate through node list
-divs.forEach((div) => {
-    //Add event listener for hover on each square
-    div.addEventListener('mouseover', () => {
-        //Change background color of square to random RGB value
-        div.style.backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
-    });
-});
